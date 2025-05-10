037c6e9e41d285ef251bbb00cd4128b69f779959
Onyekachukwu Muoghalu <onyekachukwumuoghalu@gmail.com>
2025-05-10 10:19:01 -0500

    Integration of Db has stated Running tests to ensure server side can create, insert, update, and delete from the DB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,6 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import cors from 'cors';
+import { Request, Response } from "express"; 
 import { config } from "dotenv";
 import { healthRouter } from "./routes/health.route";
 
@@ -6,9 +8,18 @@ config(); // loads .env
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/health", healthRouter);
 
 // 404 fallback
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, Express with TypeScript!');
+});
+
+app.get('/Api_test', (req: Request, res: Response) => {
+  res.send('Worked')
+});
+
 app.use((_req: Request, res: Response) => {
   res.status(404).json({ message: "Not found" });
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from 'react';
 
 function App() {
-  return <p>filler</p>;
+  const [api_call, SetApiCall] = useState("Not Worked"); 
+
+  useEffect(() => {
+    const fetchdata = async () => {
+      try{
+        const response = await fetch('http://localhost:3000/Api_test');
+        if(!response.ok){
+          throw new Error(`HTTP error status: ${response.status}`);
+        }
+        const Api_res = await response.text();
+        console.log(Api_res);
+        SetApiCall(Api_res);
+      }
+      catch (e){
+        console.log(`Error: ${e}`)
+      }
+    };
+    fetchdata();
+  }, []);
+
+  return <p>filler: {api_call}</p>;
 }
 
 export default App;
